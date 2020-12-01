de68ac97994d0891bcee8b03eeaf7dd23297522a
sreekanthm <sreekanth.kanth@gmail.com>
2020-12-01 10:52:52 +0530

    updated provider

--- providers.tf
+++ providers.tf
@@ -2,10 +2,10 @@ provider "aws" {
     region = var.region
 }
 
-terraform {
-  backend "s3"{
-      bucket = "javahome-tf-12121"
-      key    = "terraform.tfstate"
-      region = "eu-west-1"
-  }
-}
+# # terraform {
+# #   backend "s3"{
+# #       bucket = "javahome-tf-12121"
+# #       key    = "terraform.tfstate"
+# #       region = "eu-west-1"
+# #   }
+# }
--- variables.tf
+++
@@ -1,9 +0,0 @@
-variable "region" {
-    default = "eu-west-1"
-}
-variable "vpc_cidr" {
-    default = "10.20.0.0/16"
-}
-variable "tenancy" {
-    default = "default"
-}
